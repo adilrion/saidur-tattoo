a2d86e51c30c9a7815ea3ef106a505ea36a4b25d
AdilMahmoudRion <adilmahmoudrion@gmail.com>
2022-10-09 23:27:00 +0600

    confirmation message added

--- src/Components/Pages/Contact/Contact.js
+++ src/Components/Pages/Contact/Contact.js
@@ -11,6 +11,7 @@ const Contact = () => {
 
   const form = useRef();
   const [massage, setMassage] = useState(false)
+  
 
   const sendEmail = (e) => {
     e.preventDefault();
@@ -71,6 +72,9 @@ const Contact = () => {
             </div>
           </div>
           <div className="col-span-1 my-[15px]">
+
+
+
             <form ref={form} onSubmit={sendEmail}>
               <div className="relative z-0 w-full group ">
                 <input
@@ -119,12 +123,31 @@ const Contact = () => {
                   Type Your Massage..
                 </label>
               </div>
-              <button
-                type="submit"
-                className="text-white bg-[#e8dc36]  focus:ring-4 focus:ring-[#e8dc36] font-medium text-md w-full md:w-fit px-10 py-2.5 text-center  gap-1 inline-flex justify-center"
-              >
-                Send <RiSendPlaneLine />
-              </button>
+              {
+                massage ? ( <div class="px-8 py-6 massage-card text-white flex justify-between rounded">
+                <div class="flex items-center">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-7 w-7 mr-6" viewBox="0 0 20 20" fill="currentColor">
+                        <path
+                            d="M2 10.5a1.5 1.5 0 113 0v6a1.5 1.5 0 01-3 0v-6zM6 10.333v5.43a2 2 0 001.106 1.79l.05.025A4 4 0 008.943 18h5.416a2 2 0 001.962-1.608l1.2-6A2 2 0 0015.56 8H12V4a2 2 0 00-2-2 1 1 0 00-1 1v.667a4 4 0 01-.8 2.4L6.8 7.933a4 4 0 00-.8 2.4z"
+                        />
+                    </svg>
+                    <p>Congratulation! Email send was successful!</p>
+                </div>
+                <button onClick={()=> setMassage(false)} class="text-green-100 hover:text-white">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                    </svg>
+                </button>
+            </div>):(
+                  <button
+                  type="submit"
+                  className="text-white bg-[#e8dc36]  focus:ring-4 focus:ring-[#e8dc36] font-medium text-md w-full md:w-fit px-10 py-2.5 text-center  gap-1 inline-flex justify-center"
+                >
+                  Send <RiSendPlaneLine />
+                </button>
+                )
+              }
+             
             </form>
           </div>
         </div>
